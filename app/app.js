4a7b2511a68785db15a7b82070843323c91be192
teenoh <valentino@grit.systems>
2017-09-05 02:39:48 +0100

    added basic styling

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import Welcome from './screens/Welcome'
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <Welcome />
+        )
+    }
+}
--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -1,29 +1,74 @@
 import React, { Component } from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, TouchableOpacity, Button } from "react-native";
 
-class Welcome extends Component {
+export default class Welcome extends Component {
   render() {
-    <View>
-      <View>
-        <Text>Hacker News</Text>
-        <Text>Get up to date news on tech stuff</Text>
-      </View>
+    return (
+      <View style={styles.containerStyle}>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerTextStyle}>Hacker News</Text>
+          <Text>Get up to date news on tech stuff</Text>
+        </View>
 
-      <View>
-        <Button />
-        <Button />
-      </View>
+        <View style={styles.buttonView}>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.buttonTextStyle}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.buttonTextStyle}> Register </Text>
+          </TouchableOpacity>
+        </View>
 
-      <View>
-        <Text> Hacker News clone</Text>
+        <View style={{ flex: 0.5 }}>
+          <Text> Hacker News clone</Text>
+        </View>
       </View>
-    </View>;
+    );
   }
 }
 
 const styles = {
-    containerStyle:{},
-    headerTextStyle:{},
-    buttonStyle: {},
-    bottomTextStyle:{}
-}
+  containerStyle: {
+    flex: 1,
+    backgroundColor: "#ff6600"
+  },
+
+  headerTextView: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue"
+  },
+
+  headerTextStyle: {
+    fontSize: 30,
+    color: "white"
+  },
+
+  buttonView: {
+    flex: 0.3,
+    flexDirection: "row"
+  },
+
+  buttonStyle: {
+    flex: 1,
+    alignSelf: "stretch",
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#007aff",
+    marginLeft: 5,
+    marginRight: 5
+  },
+
+  buttonTextStyle: {
+    alignSelf: 'center',
+    color: "#007aff",
+    fontSize: 16,
+    fontWeight: "600",
+    paddingTop: 10,
+    paddingBottom: 10
+},
+
+  bottomTextStyle: {}
+};
