d32537de67e4a7d604712575489408ed2b385a08
teenoh <valentino@grit.systems>
2017-09-05 01:44:26 +0100

    skeleton for welcome screen

---
+++ app/screens/Welcome.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Text, View, Button } from "react-native";
+
+class Welcome extends Component {
+  render() {
+    <View>
+      <View>
+        <Text>Hacker News</Text>
+        <Text>Get up to date news on tech stuff</Text>
+      </View>
+
+      <View>
+        <Button />
+        <Button />
+      </View>
+
+      <View>
+        <Text> Hacker News clone</Text>
+      </View>
+    </View>;
+  }
+}
