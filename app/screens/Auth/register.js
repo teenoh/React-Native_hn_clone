022d260d38a163f7c0a6d2d62cbd823a85de0025
dansmog <juwonanthony@gmail.com>
2017-09-11 21:52:55 +0100

    add navigation

---
+++ app/screens/Auth/register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+export default class Register extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the Register Page</Text>
+      </View>
+    );
+  }
+}
+
