5a6aded5003c60fa35735f9a13714d49e0ba085a
Ugbala Valentino <ugbalaachinike@yahoo.com>
2017-10-02 20:20:56 +0100

    Revert "PR for welcome screen"

--- index.android.js
+++ index.android.js
@@ -1,5 +1,43 @@
-import { AppRegistry } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-import App from './app/main';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './app/app'
 
-AppRegistry.registerComponent('hnclone', () => App);
+export default class hnclone extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('hnclone', () => hnclone);
